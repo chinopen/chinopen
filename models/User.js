3a33d353cf2eadc8f4843f8e9e8a33cdaaa74910
Carlos Llorente <llorenteyuste@hotmail.com>
2018-04-23 17:37:23 +0200

    prueba

--- models/User.js
+++ models/User.js
@@ -5,19 +5,14 @@ const userSchema = new Schema({
   username: String,
   email: String,
   password: String,
-  location: { type: { type: String }, coordinates: [Number] },
   isShop: { type: Boolean, default: false },
-  open: String,
-  close: String
+  //location: { type: { type: String }, coordinates: [Number] },
+  open: { type: String, default: null },
+  close: { type: String, default: null },
 });
 userSchema.index({ location: "2dsphere" });
 userSchema.set('timestamps', true);
 
 const User = mongoose.model("User", userSchema);
-
-user.on('index', function(error) {
-    // "_id index cannot be sparse"
-    console.log(error);  
-});
 
 module.exports = User;
