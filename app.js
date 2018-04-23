3371dae5b95c1369cbb4c8c4c558f188597b57e1
Carlos Llorente <llorenteyuste@hotmail.com>
2018-04-23 18:26:44 +0200

    sin saas

--- app.js
+++ app.js
@@ -66,18 +66,12 @@ app.use((req, res, next) => {
 
 // Express View engine setup
 
-app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true
-  })
-);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
 
 // default value for title local
 app.locals.title = "chinOpen";
