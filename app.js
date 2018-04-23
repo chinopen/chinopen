4f9e33ada63249e6eeef4140bd7da40d97177f94
Beltran <beltran_xv@hotmail.com>
2018-04-23 13:58:26 +0200

    prueba merge

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 const dbURL = process.env.DBURL;
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
-
 mongoose.Promise = Promise;
 mongoose
-  .connect(dbURL, {useMongoClient: true})
+  .connect(dbURL, { useMongoClient: true })
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -41,7 +43,7 @@ app.use(
     cookie: { maxAge: 60 * 60 * 24 * 2 * 1000 }, //2 days
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60 * 2 *1000 // 2 day
+      ttl: 24 * 60 * 60 * 2 * 1000 // 2 day
     })
   })
 );
@@ -58,23 +60,21 @@ app.use((req, res, next) => {
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'chinOpen';
-
+app.locals.title = "chinOpen";
 
 const index = require("./routes/index");
 app.use("/", index);
@@ -84,6 +84,5 @@ app.use("/auth", authRoutes);
 
 const userRoutes = require("./routes/userRoutes");
 app.use("/user", userRoutes);
-
 
 module.exports = app;
