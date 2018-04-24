d89533811cb8b5ca2cc2deeda570a8e62b20cd78
Carlos Llorente <llorenteyuste@hotmail.com>
2018-04-24 21:13:08 +0200

    Todo junto

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,78 +9,97 @@ const googleMapsClient = require("@google/maps").createClient({
 
 const router = express.Router();
 
-  router.get("/shops", (req, res, next) => {
-    res.render("user/shops", {
-      errorMessage: ""
-    });
+router.get("/shops", (req, res, next) => {
+  res.render("user/shops", {
+    errorMessage: ""
   });
-  router.post("/changePass", (req, res, next) =>{ 
-    const password = req.body.password;
-    const pass = bcrypt.hashSync(password, salt)
-    const userId = req.session.currentUser._id;
-    const update = {password:pass}
-    User.findByIdAndUpdate(userId,update)
-    .then(user=>{
+});
+router.post("/changePass", (req, res, next) => {
+  const password = req.body.password;
+  const pass = bcrypt.hashSync(password, salt)
+  const userId = req.session.currentUser._id;
+  const update = {
+    password: pass
+  }
+  User.findByIdAndUpdate(userId, update)
+    .then(user => {
       user.password = pass;
-            res.redirect('/');            
+      res.redirect('/');
     })
-  })
-  
-  router.post("/changeName", (req, res, next) =>{ 
-    const username = req.body.username;
-    const name = username
-    const userId = req.session.currentUser._id;
-    const update = {username:name}
-    User.findByIdAndUpdate(userId,update)
-    .then(user=>{
+})
+
+router.post("/changeName", (req, res, next) => {
+  const username = req.body.username;
+  const name = username
+  const userId = req.session.currentUser._id;
+  const update = {
+    username: name
+  }
+  User.findByIdAndUpdate(userId, update)
+    .then(user => {
       user.username = name;
-            res.redirect('/');            
+      res.redirect('/');
     })
-  })
+})
 
-  router.post("/changeOpen", (req, res, next) =>{ 
-    const open = req.body.open;
-    const time = open
-    const userId = req.session.currentUser._id;
-    const update = {open:time}
-    User.findByIdAndUpdate(userId,update)
-    .then(user=>{
+router.post("/changeOpen", (req, res, next) => {
+  const open = req.body.open;
+  const time = open
+  const userId = req.session.currentUser._id;
+  const update = {
+    open: time
+  }
+  User.findByIdAndUpdate(userId, update)
+    .then(user => {
       user.open = time;
-            res.redirect('/');            
+      res.redirect('/');
     })
+})
+router.get('/userFirst', (req, res, next) => {
+  User.find({
+    isCoords: true
+  }).then((users) => {
+    res.render("user/userFirst", {
+      users: JSON.stringify(users)
+    });
   })
-  router.get('/userFirst', (req, res, next) => {
-    User.find({isCoords:true}).then( (users)  => {
-      res.render("user/userFirst",{users:JSON.stringify(users)});
-    })
-  });
+});
 
-  router.post("/changeClose", (req, res, next) =>{ 
-    const close = req.body.close;
-    const time = close
-    const userId = req.session.currentUser._id;
-    const update = {close:time}
-    User.findByIdAndUpdate(userId,update)
-    .then(user=>{
+router.post("/changeClose", (req, res, next) => {
+  const close = req.body.close;
+  const time = close
+  const userId = req.session.currentUser._id;
+  const update = {
+    close: time
+  }
+  User.findByIdAndUpdate(userId, update)
+    .then(user => {
       user.close = time;
-            res.redirect('/');            
+      res.redirect('/');
     })
-  })
+})
 
-  router.post("/addAddress", (req, res, next) =>{ 
-    const address = req.body.direccion;
-    
-    let lat;
-    let lng;
-    googleMapsClient.geocode({address})
+router.post("/addAddress", (req, res, next) => {
+  const address = req.body.direccion;
+
+  let lat;
+  let lng;
+  googleMapsClient.geocode({
+      address
+    })
     .asPromise()
     .then(data => {
-        lat = data.json.results[0].geometry.viewport.northeast.lat;
-        lng = data.json.results[0].geometry.viewport.northeast.lng;
-        const userId = req.session.currentUser._id;
-        const update = { loc: { coordinates: [lat, lng] }, isCoords: true }
-        // User.findOne({ _id: userId})
-        User.findByIdAndUpdate(userId, update)
+      lat = data.json.results[0].geometry.viewport.northeast.lat;
+      lng = data.json.results[0].geometry.viewport.northeast.lng;
+      const userId = req.session.currentUser._id;
+      const update = {
+        loc: {
+          coordinates: [lat, lng]
+        },
+        isCoords: true
+      }
+      // User.findOne({ _id: userId})
+      User.findByIdAndUpdate(userId, update)
         .then(user => {
 
           res.redirect("/user/userFirst")
@@ -89,8 +108,16 @@ const router = express.Router();
     .catch(error => {
       console.log(error)
     })
+
+
 })
 
+router.get("/place/:id",(req, res,next) => {
+  user.findById(req.params.id)
+  .then (place => {
+   res.render("profile", place)
+  
+  })
+ })
 
-
-  module.exports = router;
+module.exports = router;
